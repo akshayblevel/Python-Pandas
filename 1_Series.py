a05ab0b8b5f45eaac75ea8005d2cef605a481483
Akshaykumar S Patel <38757471+akshayblevel@users.noreply.github.com>
2025-08-12 08:05:19 +0530

    Series

---
+++ 1_Series.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+ratings = [14, 10, 81, 16, 11, 84]
+
+series = pd.Series(ratings)
+
+print(series)
+
+'''
+OUTPUT:
+
+0    14     
+1    10     
+2    81     
+3    16     
+4    11     
+5    84     
+dtype: int64
+'''
